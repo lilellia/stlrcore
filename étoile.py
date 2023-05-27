2bca542423fb4dc1e8c8a13a13751dfa27f90c4d
Lily Ellington <pixielf.0@gmail.com>
2023-05-26 21:11:00 -0400

    fix étoile to optionally  allow file passed in from command line

--- étoile.py
+++ étoile.py
@@ -7,7 +7,7 @@ from stlr.transcribe import Transcription
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument("file", type=Path, default=None)
+    parser.add_argument("-a", "--audio", dest="file", type=Path, default=None)
     args = parser.parse_args()
 
     if args.file is None:
